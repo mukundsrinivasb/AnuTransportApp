8589a4b121232456ac52e41361038e7658c95f1b
Mukund Balaji Srinivas <mukundbalaji.srinivas@anu.edu.au>
2022-01-14 15:06:36 +0530

    Some changes to the Icon

--- app/src/main/java/com/example/anubusroutes/SignInActivity.java
+++ app/src/main/java/com/example/anubusroutes/SignInActivity.java
@@ -10,7 +10,6 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -24,7 +23,7 @@ public class SignInActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         mAuth = FirebaseAuth.getInstance();
         Log.w(TAG, "onCreate: words");
     }
