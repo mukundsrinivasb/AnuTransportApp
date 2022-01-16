96301ab921646391ca38035b6112e28beb62fac9
Mukund Balaji Srinivas <mukundbalaji.srinivas@anu.edu.au>
2022-01-17 00:49:18 +0530

    Splash Screen works yaay!!

--- app/src/main/java/com/example/anubusroutes/SignInActivity.java
+++ app/src/main/java/com/example/anubusroutes/SignInActivity.java
@@ -1,9 +1,11 @@
 package com.example.anubusroutes;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.util.Log;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -15,31 +17,10 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class SignInActivity extends AppCompatActivity {
-
-    public EditText emailId = findViewById(R.id.emailId);
-    public EditText password = findViewById(R.id.password);
-    public FirebaseAuth mAuth;
-    private  String TAG = "SignInAct";
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setTheme(R.style.Theme_ANUBusRoutes);
         setContentView(R.layout.activity_login);
-        mAuth = FirebaseAuth.getInstance();
-        Log.w(TAG, "onCreate: words");
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser != null){
-            Log.i(TAG, "user: "+currentUser.getEmail());
-        }
-        else{
-
-        }
-
-
     }
 }
