c594a4243708959d5dabc503209bdd6b1b1d5be6
Mukund Balaji Srinivas <mukundbalaji.srinivas@anu.edu.au>
2022-01-20 18:18:15 +0530

    Some changes to the button

--- app/src/main/java/com/example/anubusroutes/SignInActivity.java
+++ app/src/main/java/com/example/anubusroutes/SignInActivity.java
@@ -1,26 +1,49 @@
 package com.example.anubusroutes;
 
-import androidx.annotation.NonNull;
+
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class SignInActivity extends AppCompatActivity {
+    public static String TAG = "Login Screen/";
+    protected EditText email;
+    protected EditText password;
+    public Button loginButton;
     @Override
-
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        email = findViewById(R.id.emailId);
+        password = findViewById(R.id.password);
+        loginButton = findViewById(R.id.loginbutton);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        loginButton.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View view) {
+               if(email.getText().toString().isEmpty() || email.getText().toString().isEmpty())
+                   try {
+                       throw new EmptyFieldException("login / password cannot be blank");
+                   } catch (EmptyFieldException e) {
+                       e.printStackTrace();
+                   }
+                   Log.i(TAG,email.getText().toString());
+               }
+
+            }
+
+        );
+
     }
 }
