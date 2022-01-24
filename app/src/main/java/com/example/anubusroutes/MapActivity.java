fce0e3bb61c9cbbeec9b848e73cc90758618398e
Mukund Balaji Srinivas <mukundbalaji.srinivas@anu.edu.au>
2022-01-25 00:33:29 +0530

    MapView working. Have to figure out how to map fetch the open street maps from the internet. Also have to figure out how to input a Map Position

--- app/src/main/java/com/example/anubusroutes/MapActivity.java
+++ app/src/main/java/com/example/anubusroutes/MapActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import org.osmdroid.util.BoundingBox;
 import org.osmdroid.views.MapView;
 
 public class MapActivity extends AppCompatActivity {
@@ -22,6 +23,14 @@ public class MapActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        MapView tMap = findViewById(R.id.)
+        MapView tMap = findViewById(R.id.MapView);
+        tMap.post(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        tMap.zoomToBoundingBox(new BoundingBox(48, -2, 46, 5),true);
+                    }
+                }
+        );
     }
 }
